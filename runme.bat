6a862d9226f1e7601ef306d457a9b72168a02b86
Joe Finn (YR2004) <FinnJ01@horsforthschool.org>
2017-07-18 14:23:00 +0100

    Automated Commit

--- runme.bat
+++ runme.bat
@@ -1,3 +1,4 @@
+C:\Users\finnj01\Desktop\wang\Git\bin\git.exe pull
 C:\Users\finnj01\Desktop\wang\Git\bin\git.exe add -A
 C:\Users\finnj01\Desktop\wang\Git\bin\git.exe commit -m "Automated Commit"
 C:\Users\finnj01\Desktop\wang\Git\bin\git.exe push
